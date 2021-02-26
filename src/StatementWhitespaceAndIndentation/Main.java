3badca19aa15ee9d04aa8aad626addd5294ffb07
ZitronCrazy <zitro5@protonmail.com>
2021-02-26 23:29:00 +0100

    StatementsWhitespaceAndIndentation

---
+++ src/StatementWhitespaceAndIndentation/Main.java
@@ -0,0 +1,18 @@
+package StatementWhitespaceAndIndentation;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable=50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+                " another" +
+                " still more.");
+
+        //This would be confusing
+        int anotherVariable = 50;myVariable--;System.out.println("This is another one");
+    }
+}
